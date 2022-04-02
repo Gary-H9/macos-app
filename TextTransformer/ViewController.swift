68df75c552f1ad7ef4749c40158189d2cdbe2c27
Gary-H9 <26419401+Gary-H9@users.noreply.github.com>
2022-04-02 13:41:37 +0100

    Initial Commit

---
+++ TextTransformer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TextTransformer
+//
+//  Created by Gary Henderson on 02/04/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
